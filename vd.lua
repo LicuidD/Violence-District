09b13b207cfcdae40992373ce3039f85b4c03069
LicuidD <timurlohtimur228@gmail.com>
2026-09-03 23:45:35 +0400

    Update vd.lua

--- vd.lua
+++ vd.lua
@@ -1,26 +1,7 @@
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
-local UserInputService = game:GetService("UserInputService")
-
-UserInputService.InputBegan:Connect(function(input)
-	if input.KeyCode == Enum.KeyCode.V then
-		local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
-		local Humanoid = Character:WaitForChild("Humanoid") 
-		if Humanoid.WalkSpeed ~= 32 then -- Скорость
-			Humanoid.WalkSpeed = 32
-		else
-			Humanoid.WalkSpeed = 16
-		end
-	end
-end)
 
 while true do
-	--[[
-	if game.Workspace.Camera.FieldOfView ~= 110 then -- FOV
-		game.Workspace.Camera.FieldOfView = 110
-	end
-	--]]
-	
 	for _, v in Players:GetPlayers() do -- Игроки
 		local character = v.Character
 
@@ -45,45 +26,6 @@ while true do
 					v:Destroy()
 				end
 			end
-		end
-	end
-	for _, v in game.Workspace.Map:GetChildren() do 
-		if v.Name == "Palletwrong" and not v:FindFirstChild("Highlight") then --Паллеты
-			local Highlight = Instance.new("Highlight")
-			Highlight.FillColor = Color3.new(1, 0.54902, 0)
-			Highlight.OutlineColor = Color3.new(1, 0.54902, 0)
-			Highlight.OutlineTransparency = 0.3
-			Highlight.Adornee = v
-			Highlight.Parent = v
-		elseif v.Name == "Gate" then -- Ворота
-			for _, v in v:GetChildren() do
-				if v.Name == "ExitLever" and not v:FindFirstChild("Highlight") then
-					local Highlight = Instance.new("Highlight")
-					Highlight.FillColor = Color3.new(0, 0.764706, 1)
-					Highlight.OutlineColor = Color3.new(0, 0.764706, 1)
-					Highlight.OutlineTransparency = 0.3
-					Highlight.Adornee = v
-					Highlight.Parent = v
-				end
-			end
-		elseif v.Name == "Generators" then -- Генераторы
-			for _, v in v:GetChildren() do
-				if v.Name == "Generator" and not v:FindFirstChild("Highlight") then
-					local Highlight = Instance.new("Highlight")
-					Highlight.FillColor = Color3.new(1, 0.564706, 0.0431373)
-					Highlight.OutlineColor = Color3.new(1, 0.564706, 0.0431373)
-					Highlight.OutlineTransparency = 0.3
-					Highlight.Adornee = v
-					Highlight.Parent = v
-				end
-			end
-		elseif v.Name == "Window" and not v:FindFirstChild("Highlight") then -- Окна
-			local Highlight = Instance.new("Highlight")
-			Highlight.FillColor = Color3.new(1, 1, 1)
-			Highlight.OutlineColor = Color3.new(1, 1, 1)
-			Highlight.OutlineTransparency = 0.3
-			Highlight.Adornee = v
-			Highlight.Parent = v
 		end
 	end
 	wait(0.2)
